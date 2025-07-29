3c71f63d301906e82225b763b80e5eccfdd96d79
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2025-07-29 13:36:04 +0530

    Create string_rune_int_float_bool.go

---
+++ beginner/string_rune_int_float_bool.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// main function where exection starts
+func main(){
+    
+    // string in double quote
+    fmt.Println("This is normal string which can have 123, #$%^&, true and false, 34.5")
+    
+    // char or rune in single quote gives value
+    fmt.Println('K') // gives 75
+    
+    // bool value
+    fmt.Println(true)
+    fmt.Println(false)
+    
+    // num and float
+    fmt.Println(34)
+    fmt.Println(116.5)
+}
