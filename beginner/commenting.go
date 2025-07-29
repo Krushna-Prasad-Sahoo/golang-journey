866e9840d42b1caf44e028edd2983cbd13289af2
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2025-07-29 12:55:22 +0530

    Create commenting.go

---
+++ beginner/commenting.go
@@ -0,0 +1,17 @@
+package main
+
+// importing the formatting library
+import "fmt"
+
+// main function where exection starts
+func main(){
+    
+    /*
+    The "fmt" library which we imported has some
+    functions to print out the strings.
+    Examples below:
+    */
+    fmt.Print("Hello Krushna")
+    fmt.Println("Welcome to Go Lang World!")
+    fmt.Printf("Wish you all the very best")
+}
