864369dfe4e299dc4b308affb7bbd645787f007a
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2025-07-29 17:04:52 +0530

    Create variable_default-values.go

---
+++ beginner/variable_default-values.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    )
+
+func main(){
+    
+    // variable declaration
+    var x int32
+    var y float64
+    var z bool
+    var a string
+    var b rune
+    
+    
+    
+    // checking default values when explicitly no values are assigned
+    fmt.Println(x)
+    fmt.Println(y)
+    fmt.Println(z)
+    fmt.Println(a)
+    fmt.Println(b)
+    
+}
