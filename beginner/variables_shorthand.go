c871a9e1a1618bc19d743693b4769dada56e1399
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2025-07-29 17:07:03 +0530

    Create variables_shorthand.go

---
+++ beginner/variables_shorthand.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    )
+
+func main(){
+    
+    // variable shorthand declaration and value assignment
+   name := "KP"
+   age := 49
+   height, weight := 5.7, 120
+    
+    
+    // checking default values when explicitly no values are assigned
+    fmt.Println(name)
+    fmt.Println(age)
+    fmt.Println(height)
+    fmt.Println(weight)
+    
+}
